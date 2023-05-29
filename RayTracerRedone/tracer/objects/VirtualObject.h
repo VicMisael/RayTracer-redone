79daa46be90722fa2880a83922dad4d5e14fcd18
VicMisael <misael1805@gmail.com>
2023-05-28 22:45:20 -0300

    Fix linux compiling bug and add optimization

--- RayTracerRedone/tracer/objects/VirtualObject.h
+++ RayTracerRedone/tracer/objects/VirtualObject.h
@@ -2,7 +2,7 @@
 #include <memory>
 #include <optional>
 
-#include "../utils/intersection.h"
+#include "../utils/Intersection.h"
 #include "../scene/Ray.h"
 #include "../scene/materials/Material.h"
 #include "../utils/Constants.h"
@@ -11,11 +11,11 @@ class VirtualObject
 {
 public: 
 	std::shared_ptr<Material> material;
-	VirtualObject(std::shared_ptr<Material> _material) 
+	explicit VirtualObject(std::shared_ptr<Material> _material)
 	{
 		material = _material;
 	};
-	virtual std::optional<intersection> intersects(const Ray &ray) const=0;
+	[[nodiscard]] virtual std::optional<intersection> intersects(const Ray &ray) const=0;
 };
 
 	
