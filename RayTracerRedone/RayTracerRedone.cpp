89baf766a8e875d1b5ff5617da4cf082ec6c889f
Victor Misael <misael1805@gmail.com>
2023-05-30 23:08:18 -0300

    disable cylinder and up resolution

--- RayTracerRedone/RayTracerRedone.cpp
+++ RayTracerRedone/RayTracerRedone.cpp
@@ -73,7 +73,7 @@ std::vector<std::shared_ptr<VirtualObject>> generateObjects()
 		std::make_shared<PhongReflective>(ColorVec(Constants::BLACK),1,25,1)));
 
 
-    objects.push_back(std::make_shared<OpenCylinder>(Vector3(0,1,0),Point3(100,0,-300),150,50,std::make_shared<PhongReflective>()));
+    //objects.push_back(std::make_shared<OpenCylinder>(Vector3(0,1,0),Point3(100,0,-300),150,50,std::make_shared<PhongReflective>()));
 
 	return objects;
 }
@@ -101,7 +101,7 @@ int main()
 
 	const ViewPlane view_plane=projection? ViewPlane(60, 60,20, 01.0f): ViewPlane(1450, 1450, 50, 01.0f);
 
-	sampler* sampler = new mt19937_point_sampler(20);
+	sampler* sampler = new mt19937_point_sampler(90);
 
 	AmbientLight ab(0, ColorVec(1.0, 1.0, 1));
 	World world(view_plane, generateObjects(), generate_vectorial_lights(), ab, {0.9 , 0.9 , 1}, sampler, projection);
@@ -109,7 +109,7 @@ int main()
 
 	const Scene scene(&world, canvas);
 
-	constexpr int32_t recursion_depth_limit = 3;
+	constexpr int32_t recursion_depth_limit = 8;
 
 	auto draw = [&]
 	{
