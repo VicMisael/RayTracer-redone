33eadcc331005beed25649c58ff2f7466dc09823
Victor Misael <misael1805@gmail.com>
2023-06-02 00:20:11 -0300

    redo plane UV

--- RayTracerRedone/tracer/objects/Plane.cpp
+++ RayTracerRedone/tracer/objects/Plane.cpp
@@ -2,34 +2,29 @@
 #include <glm/geometric.hpp>
 
 
-vec3 computePrimaryTexDir(vec3 normal)
-{
+glm::vec3 computeU(glm::vec3 normal) {
+    glm::vec3 randomVec = glm::vec3(1, 0, 0);
+    if (glm::dot(normal, randomVec) > 0.99) // they are too parallel
+        randomVec = glm::vec3(0, 1, 0);
 
-    vec3 a = cross(normal, vec3(1, 0, 0));
-    vec3 b = cross(normal, vec3(0, 1, 0));
-
-    vec3 max_ab = dot(a, a) < dot(b, b) ? b : a;
-
-    vec3 c = cross(normal, vec3(0, 0, 1));
-
-    return normalize(dot(max_ab, max_ab) < dot(c, c) ? c : max_ab);
+    return glm::normalize(glm::cross(normal, randomVec));
 }
 
-std::tuple<float,float> get_plane_uv(const Vector3 normal,const Point3 point_in_plane,const Point3 point){
-    glm::vec3 U_direction = glm::normalize(glm::cross(normal, glm::vec3(0.0f, 1.0f, 0.0f)));
+glm::vec3 computeV(glm::vec3 normal, glm::vec3 u) {
+    return glm::cross(normal, u);
+}
 
-    if (glm::length(U_direction) < 0.001f)
-        U_direction = glm::normalize(glm::cross(normal, glm::vec3(1.0f, 0.0f, 0.0f)));
+std::tuple<float,float> computeUV(glm::vec3 pointInPlane, glm::vec3 otherPoint, glm::vec3 normal) {
+    glm::vec3 u = computeU(normal);
+    glm::vec3 v = computeV(normal, u);
 
-    glm::vec3 V_direction = glm::normalize(glm::cross(normal,U_direction));
+    glm::vec3 delta = otherPoint - pointInPlane;
 
-    glm::vec3 W = point;
-    
-    float u = glm::dot(W, U_direction);
-    float v = glm::dot(W, V_direction);
+    float uCoordinate = glm::dot(delta, u);
+    float vCoordinate = glm::dot(delta, v);
 
-    return { u,v };
-   }
+    return { uCoordinate, vCoordinate };
+}
 std::optional<intersection> Plane::intersects(const Ray &ray) const
 {
 	using namespace glm;
@@ -37,7 +32,7 @@ std::optional<intersection> Plane::intersects(const Ray &ray) const
 	const float t = dot(point - ray.origin, normal) / dot_dir_normal;
 	if (t > Constants::EPSILON+0.001f) {
         const auto intersectionPoint=ray.point_at(t);
-        const auto [u,v] = get_plane_uv(normal,point, intersectionPoint);
+        const auto [u,v] = computeUV(point, intersectionPoint,normal);
 		return intersection{true,t,ray.point_at(t),normal,material.value(),u,v};
 	}
 	
