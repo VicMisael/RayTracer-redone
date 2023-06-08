7002aedbe9ce6271fa3b52243e6d583f3741a148
VicMisael <misael1805@gmail.com>
2023-06-08 18:10:59 -0300

    Update Scenario

--- RayTracerRedone/tracer/scene/light/DirectionalLight.cpp
+++ RayTracerRedone/tracer/scene/light/DirectionalLight.cpp
@@ -5,33 +5,28 @@
 #include "../Ray.h"
 #include "../World.h"
 
-Vector3 DirectionalLight::getVector(const Point3 p) const
-{
-	return direction_;
+Vector3 DirectionalLight::getVector(const Point3 p) const {
+    return direction_;
 }
 
-Vector3 DirectionalLight::getNormalizedDirection(const Point3 p) const
-{
-	return glm::normalize(getVector(p));
+Vector3 DirectionalLight::getNormalizedDirection(const Point3 p) const {
+    return glm::normalize(getVector(p));
 }
 
-ColorVec DirectionalLight::intensityAtPoint(const Point3) const
-{
-	return this->intensity * this->color;
+ColorVec DirectionalLight::intensityAtPoint(const Point3) const {
+    return this->intensity * this->color;
 }
 
-bool DirectionalLight::shadow_hit(const World& world, const Ray& outgoing) const
-{
+bool DirectionalLight::shadow_hit(const World &world, const Ray &outgoing) const {
 
 
-		const std::optional<intersection> intersection = world.hit(outgoing);
-		if (intersection)
-		{
-			const auto &intersection_data = intersection.value();
-			if( intersection_data.tmin> 0 ){
-				return true;
-			}
-		}
+    const std::optional<intersection> intersection = world.hit(outgoing);
+    if (intersection) {
+        const auto &intersection_data = intersection.value();
+        if (intersection_data.tmin > 0.01f) {
+            return true;
+        }
+    }
 
-	return false;
+    return false;
 }
